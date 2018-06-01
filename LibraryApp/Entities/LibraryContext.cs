e0b02558745c73f2ac4b799114a2cc2b73574b59
Mizu Rahman <dhmizu95@gmail.com>
2018-06-01 12:31:33 +0600

    Initialize project files

---
+++ LibraryApp/Entities/LibraryContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApp.Entities
+{
+    public sealed class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+           : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
