40f10f3cf0f8db11eb50fba2bcb1b14578c0b031
Mizu Rahman <dhmizu95@gmail.com>
2018-06-06 15:38:32 +0600

    Add xml serializer input formatter

--- LibraryApp/Startup.cs
+++ LibraryApp/Startup.cs
@@ -22,7 +22,10 @@ namespace LibraryApp {
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc()
-                .AddMvcOptions(opt => opt.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));
+                .AddMvcOptions(opt => {
+                    opt.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    opt.InputFormatters.Add(new XmlDataContractSerializerInputFormatter());
+                });
 
             services.AddAutoMapper(typeof(Startup));
 
